a0b78e3cdf740a52da4039e8df31554efa25cb89
Furkan Cansever <f.cansever67@gmail.com>
2021-03-09 20:58:30 +0300

    some minor changes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from "express";
-import process from "process";
 import bodyParser from "body-parser";
 import compression from "compression";
 import cookieParser from "cookie-parser";
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -16,7 +16,6 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 export const REDIS_URL = process.env["REDIS_URL"];
 const prod = ENVIRONMENT === "production";
 const development = ENVIRONMENT === "development";
-const test = ENVIRONMENT === "test";
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
 
@@ -24,7 +23,7 @@ export const REDIS_PORT = process.env["REDIS_PORT"];
 export const REDIS_HOST = process.env["REDIS_HOST"];
 export const REDIS_PASSWORD = process.env["REDIS_PASSWORD"];
 
-export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : test ? process.env["MONGODB_URI_TEST"] : process.env["MONGODB_URI_LOCAL"];
+export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
 
 if (!SESSION_SECRET) {
   logger.error("No client secret. Set SESSION_SECRET environment variable.");
@@ -33,11 +32,9 @@ if (!SESSION_SECRET) {
 
 if (!MONGODB_URI) {
   if (prod) {
-      logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
+    logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
   }else if (development) {
-      logger.error("No mongo connection string. Set MONGODB_URI_LOCAL environment variable.");
-  }else if (test) {
-    logger.error("No mongo connection string. Set MONGODB_URI_TEST environment variable.");
+    logger.error("No mongo connection string. Set MONGODB_URI_LOCAL environment variable.");
   }
   process.exit(1);
 }
